02799a67a11694187f4fbc88143ce3e13759a69c
Ryan <ryanrothweiler@gmail.com>
2024-01-14 21:38:27 -0800

    Day 1 part two working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::cmp::Ordering;
+use std::collections::BTreeSet;
 use std::fs;
 
 #[allow(unused_variables, unused_assignments, unused_mut)]
@@ -7,47 +9,136 @@ fn main() {
 
     let mut sum: u32 = 0;
 
-    let mut found_first = false;
-    let mut found_last = false;
-    let mut first_num: u32 = 0;
-    let mut last_num: u32 = 0;
+    let lines: Vec<&str> = contents.split('\n').collect();
 
-    for c in contents.chars() {
-        if c == '\n' {
+    for l in lines {
+        sum += handle_line(l);
+    }
 
-            let mut new = 0;
-            if found_last {
-                new = (first_num * 10) + last_num;
-            } else {
-                new = (first_num * 10) + first_num;
-            }
+    println!("{sum}");
+}
 
-            sum = sum + new;
+const NUM_WORDS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 
-            println!("{first_num}");
-            println!("{last_num}");
-            println!("break!");
+struct Entry {
+    num: u32,
+    index: u32,
+}
 
-            // reset data for next line
-            found_first = false;
-            found_last = false;
-            first_num = 0;
-            last_num = 0;
-        }
+impl Ord for Entry {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.index).cmp(&(other.index))
+    }
+}
 
-        let v = c.to_digit(10);
-        match v {
-            Some(val) => {
-                if !found_first {
-                    found_first = true;
-                    first_num = val;
-                } else {
-                    found_last = true;
-                    last_num = val;
-                }
+impl PartialOrd for Entry {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Entry {
+    fn eq(&self, other: &Self) -> bool {
+        (self.index) == (other.index)
+    }
+}
+
+impl Eq for Entry {}
+
+fn handle_line(line: &str) -> u32 {
+    let mut tree: BTreeSet<Entry> = BTreeSet::new();
+
+    let line_lower = line.to_lowercase();
+
+    // Find the words
+    for i in 0..NUM_WORDS.len() {
+        match line_lower.find(NUM_WORDS[i]) {
+            Some(ind) => {
+                tree.insert(Entry {
+                    num: (i + 1) as u32,
+                    index: ind as u32,
+                });
             }
             None => {}
         }
-        println!("{sum}");
+
+        match line_lower.rfind(NUM_WORDS[i]) {
+            Some(ind) => {
+                tree.insert(Entry {
+                    num: (i + 1) as u32,
+                    index: ind as u32,
+                });
+            }
+            None => {}
+        }
     }
+
+    // Find the character numbers
+    {
+        let mut i = 0;
+        for c in line_lower.chars() {
+            let v = c.to_digit(10);
+            match v {
+                Some(val) => {
+                    tree.insert(Entry { num: val, index: i });
+                }
+                None => {}
+            }
+
+            i = i + 1;
+        }
+    }
+
+    if tree.len() == 0 {
+        eprintln!("Emptry, tree. No numbers on this line?");
+        return 0;
+    }
+
+    if tree.len() == 1 {
+        return (tree.first().unwrap().num * 10) + tree.first().unwrap().num;
+    } else {
+        return (tree.first().unwrap().num * 10) + tree.last().unwrap().num;
+    }
+}
+
+#[test]
+fn numbers_ends() {
+    assert_eq!(handle_line("1asfadf1"), 11);
+}
+
+#[test]
+fn numbers_hidden() {
+    assert_eq!(handle_line("aa1sfa1d2fff3"), 13);
+}
+
+#[test]
+fn numbers_single() {
+    assert_eq!(handle_line("2asfadf"), 22);
+}
+
+#[test]
+fn words_ends() {
+    assert_eq!(handle_line("oneasfadtwo"), 12);
+}
+
+#[test]
+fn words_extra() {
+    assert_eq!(handle_line("oneasonefadtwothree"), 13);
+}
+
+#[test]
+fn mixed_numbers_ends() {
+    assert_eq!(handle_line("1asonefadtwothree9"), 19);
+}
+
+#[test]
+fn mixed_ends() {
+    assert_eq!(handle_line("asfivefadtwothree9"), 59);
+}
+
+#[test]
+fn mixed_hidden() {
+    assert_eq!(handle_line("asfivefadtwothree9theeeeee"), 59);
 }
