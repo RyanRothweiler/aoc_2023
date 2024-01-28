dddd19690d73307ea781fa30c65f2ba1b15ca725
Ryan <ryanrothweiler@gmail.com>
2024-01-27 23:33:03 -0800

    Day 8 working

--- src/day_eight.rs
+++ src/day_eight.rs
@@ -23,6 +23,41 @@ struct Node {
 const NODE_START: &str = "AAA";
 const NODE_END: &str = "ZZZ";
 
+fn path_length(starting_id: &str, instructions: &str, map: &HashMap<String, Node>) -> i32 {
+    let mut node_current: &Node = map.get(starting_id).unwrap();
+    let mut count: i32 = 0;
+
+    loop {
+        let mut instruction_chars = instructions.trim().chars();
+
+        if node_current.key.ends_with('Z') {
+            break;
+        }
+
+        loop {
+            match instruction_chars.next() {
+                Some(c) => {
+                    let mut next_id: &str = "";
+                    if c == 'L' {
+                        next_id = &node_current.left;
+                    } else if c == 'R' {
+                        next_id = &node_current.right;
+                    } else {
+                        return 0;
+                        //return Err("Unknown step".to_string());
+                    }
+
+                    count += 1;
+                    node_current = map.get(next_id).expect("No node for key {next_id}");
+                }
+                None => break,
+            }
+        }
+    }
+
+    return count;
+}
+
 fn steps_count(file: &str) -> Result<i32, String> {
     let contents = std::fs::read_to_string(file).unwrap();
 
@@ -37,14 +72,11 @@ fn steps_count(file: &str) -> Result<i32, String> {
             continue;
         }
 
-        //println!("{line}");
-
         let eq: Vec<&str> = line.split('=').collect();
 
         let key: String = eq[0].trim().to_string();
         let steps: Vec<&str> = eq[1].split(',').collect();
 
-        //let left = steps[0].trim().chars().next().unwrap().next_back().unwrap().as_str();
         let mut chars = steps[0].trim().chars();
         chars.next();
         let left = chars.as_str();
@@ -52,8 +84,6 @@ fn steps_count(file: &str) -> Result<i32, String> {
         let mut chars = steps[1].trim().chars();
         chars.next_back();
         let right = chars.as_str();
-
-        //println!("{key} {left} {right}");
 
         let n = Node {
             key: key.clone(),
@@ -67,49 +97,40 @@ fn steps_count(file: &str) -> Result<i32, String> {
     let mut node_current: &Node = map.get(NODE_START).expect("Missing starting node AAA");
     let mut count: i32 = 0;
 
-    loop {
-        let mut instruction_chars = instructions.trim().chars();
+    // find all starting nodes
+    let mut currents: Vec<&Node> = vec![];
+    for (key, value) in &map {
+        if key.ends_with('A') {
+            //currents.push(value);
+            let v = path_length(key, instructions, &map);
 
-        if node_current.key == NODE_END {
-            break;
-        }
-
-        loop {
-            match instruction_chars.next() {
-                Some(c) => {
-
-                    let mut next_id: &str = "";
-                    if c == 'L' {
-                        next_id = &node_current.left;
-                    } else if c == 'R' {
-                        next_id = &node_current.right;
-                    } else {
-                        return Err("Unknown step".to_string());
-                    }
-
-                    count += 1;
-                    //println!("step {c} moving to {next_id}");
-                    node_current = map.get(next_id).expect("No node for key {next_id}");
-                }
-                None => break,
-            }
+            // calc lcm of the paths
+            println!("{v}");
         }
     }
 
-    Ok(count)
+    Ok(0)
 }
 
 #[test]
-fn sample(){ 
+fn sample() {
     assert_eq!(steps_count("resources/day_8/day_8_sample.txt").unwrap(), 2);
 }
 
 #[test]
-fn sample_cycle(){ 
-    assert_eq!(steps_count("resources/day_8/day_8_sample_cycle.txt").unwrap(), 6);
+fn sample_cycle() {
+    assert_eq!(
+        steps_count("resources/day_8/day_8_sample_cycle.txt").unwrap(),
+        6
+    );
 }
 
+/*
 #[test]
-fn part_one_answer(){ 
-    assert_eq!(steps_count("resources/day_8/day_8_input.txt").unwrap(), 19199);
+fn part_one_answer() {
+    assert_eq!(
+        steps_count("resources/day_8/day_8_input.txt").unwrap(),
+        19199
+    );
 }
+*/
